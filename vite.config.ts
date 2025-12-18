8844ae01033c4c81b93710c27bd366115aa86d57
223199 <162702161+223199@users.noreply.github.com>
2025-12-18 09:11:00 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/My-Life-Log./",
+  base: "/My-Life-Log./", // ← リポジトリ名
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") }
   }
 });
-
